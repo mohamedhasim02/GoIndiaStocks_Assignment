1f654cb73105921dd3695f75c3fdbe12118649fc
Mohamed Hasim <147535511+mohamedhasim02@users.noreply.github.com>
2024-03-19 20:36:48 +0530

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Navbar from './components/navbar'
+import SimpleSidebar from './components/sidebar'
+import Stock from './components/stock'
+
+export default function Home() {
+
+  return (
+    <>
+   <Navbar/>
+   <Stock/>
+   <SimpleSidebar/>
+    </>
+    
+  )
+}
